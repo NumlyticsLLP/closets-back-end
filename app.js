df10adc93040acc7a5c2af8030bb592e243904d1
Dhruv-Numlytics <dhruv.patel@numlytics.com>
2025-10-17 17:33:08 +0530

    API key

--- app.js
+++ app.js
@@ -40,8 +40,8 @@ app.set('views', path.join(__dirname, 'views'));
 //app.use(express.urlencoded({ extended: true }));
 
 
-app.use(express.json({ limit: "26mb" })); // default is only ~1mb
-app.use(express.urlencoded({ limit: "26mb", extended: true }));
+app.use(express.json({ limit: "150mb" })); // default is only ~1mb
+app.use(express.urlencoded({ limit: "150mb", extended: true }));
  
 
 app.use('/api/user', loginRoutes);
