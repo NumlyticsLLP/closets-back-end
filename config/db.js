7fc556a605de0f8d6856cc4f3b248dc05a9c1abb
Nidhey-numlytics <nidhey.shah@numlytics.com>
2025-12-11 17:57:02 +0530

    Configure SSL for MySQL database connection
    Added SSL options for Azure database connection.

--- config/db.js
+++ config/db.js
@@ -3,7 +3,14 @@ require('dotenv').config();
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
-  dialect: 'mysql', 
+  dialect: 'mysql',
+  dialectModule: require('mysql2'),
+  dialectOptions: {
+    ssl: {
+      rejectUnauthorized: false,  // Allows Azure SSL without CA file
+    }
+  },
+  logging: false,
 });
 
 const db = {};
